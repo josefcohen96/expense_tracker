bda0fb5164cae69cc549cc8eeea58cd8e95f9853
josefcohen96 <josef244@gmail.com>
2025-09-26 17:15:16 +0300

    delete db

--- app/backend/app/db.py
+++ app/backend/app/db.py
@@ -251,7 +251,7 @@ def initialise_database() -> None:
 
     # Insert default data if tables are empty
     if not cur.execute("SELECT COUNT(*) FROM categories").fetchone()[0]:
-        for _cat in ("משכורת", "קליניקה", "בריאות", "חסכונות", "פנאי", "הוצאות בית", "רכב", "תחבורה"):
+        for _cat in ("משכורת", "קליניקה", "בריאות", "חסכונות", "פנאי", "הוצאות בית", "רכב", "תחבורה","אוכל בחוץ"):
             cur.execute("INSERT INTO categories (name) VALUES (?)", (_cat,))
 
     if not cur.execute("SELECT COUNT(*) FROM users").fetchone()[0]:
